54a93fb949b7fb40c888aeaf5d2836aa58e0faaa
Aakriti0318 <136609419+Aakriti0318@users.noreply.github.com>
2024-12-08 22:24:55 +0530

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -28,29 +28,3 @@ public class Main {
         e1.calculateWage(normalWorkDay,salPerHour);
         String k=e1.toString();
         System.out.println(k);
-//        Scanner sc = new Scanner(System.in);
-//        System.out.println("Welcome to Line Comparison Computation\n");
-//        // Input coordinates for Line 1
-//        System.out.println("Enter coordinates for Line 1 (x1, y1, x2, y2): ");
-//        double x1 = sc.nextDouble();
-//        double y1 = sc.nextDouble();
-//        double x2 = sc.nextDouble();
-//        double y2 = sc.nextDouble();
-//
-//        // Input coordinates for Line 2
-//        System.out.println("Enter coordinates for Line 2 (x3, y3, x4, y4): ");
-//        double x3 = sc.nextDouble();
-//        double y3 = sc.nextDouble();
-//        double x4 = sc.nextDouble();
-//        double y4 = sc.nextDouble();
-//
-//        // Create Line objects
-//        LineComparison line1 = new LineComparison(x1, y1, x2, y2);
-//        LineComparison line2 = new LineComparison(x3, y3, x4,y4);
-//        //finding the length of the line
-//        line1.length(x1,x2,y1,y2);
-//        line2.length(x3,x4,y3,y4);
-//        //for finding the comparison
-//        line1.compareTo(x1,x2,x3,x4,y1,y2,y3,y4);
-//
-}}
